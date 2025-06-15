8583f8aa5d3bb7bd02faa2f9231df7c5709ee4e9
Arrafi2644 <raffian2644@gmail.com>
2025-06-15 18:36:58 +0600

    Add products filter menu for small device

--- src/components/Categories/SectionProducts/SectionProducts.jsx
+++ src/components/Categories/SectionProducts/SectionProducts.jsx
@@ -7,7 +7,7 @@ const SectionProducts = ({ products }) => {
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2'>
             {
                 products.map(product => <Link key={product._id} to={`/products/${product._id}`} className='border border-gray-200 hover:shadow-md bg-white hover:shadow-gray-300'>
-                    <img className='h-64 object-cover w-full mt-2' src={product?.images[0]} alt={product?.name} />
+                    <img className='h-44 md:48 object-cover w-full mt-2' src={product?.images[0]} alt={product?.name} />
                     <div className='p-2'>
                         <h3 className='font-medium text-sm'>{product?.title}</h3>
                         <h3 className='flex items-center gap-0 font-medium text-lg'><TbCurrencyTaka />{parseInt(product?.price - product?.price * product.discount / 100)}</h3>
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -3,8 +3,10 @@ import useAllProducts from '../../hooks/useAllProducts';
 import SectionProducts from '../../components/Categories/SectionProducts/SectionProducts';
 import useCategories from '../../hooks/useCategories';
 import { Link, useLocation } from 'react-router';
-import { IoMdArrowDroprightCircle } from "react-icons/io";
+import { IoMdArrowDroprightCircle, IoMdCloseCircleOutline } from "react-icons/io";
 import { MdKeyboardArrowRight } from "react-icons/md";
+import { CiFilter } from "react-icons/ci";
+
 import "./products.css"
 import useProducts from '../../hooks/useProducts';
 
@@ -15,6 +17,7 @@ const Products = () => {
     const encodedCategory = [...pathSegments][pathSegments.length - 1]
     const decodedCategory = decodeURIComponent(encodedCategory)
     const [categoryProducts] = useProducts({ decodedCategory });
+    const [showSideMenu, setShowMenu] = useState(false)
 
     const [brand, setBrand] = useState("");
     const [service, setService] = useState("");
@@ -46,10 +49,10 @@ const Products = () => {
             </div>
 
             <div className="border-gray-300 mb-4 border-t"></div>
-            <div className='grid grid-cols-1 gap-4 lg:grid-cols-4 xl:grid-cols-5'>
+            <div className='grid grid-cols-1 gap-4 lg:grid-cols-4 xl:grid-cols-5 relative overflow-hidden'>
 
                 {/* sidebar  */}
-                <div className='border p-2 font-medium rounded-md border-gray-300'>
+                <div className={`hidden lg:block border p-2 font-medium rounded-md border-gray-300`}>
                     <div className='flex justify-between gap-2'>
                         <h2 className='font-semibold text-lg'>Filters</h2>
                         <button
@@ -167,20 +170,143 @@ const Products = () => {
                     <div className='border-b border-gray-300 my-4'></div>
                 </div>
 
+                {/* side menu for small device   */}
+                <div className={`absolute max-w-80 ${showSideMenu ? "right-0 top-0" : "-right-80 -top-0"} bg-gray-100 z-50 border p-2 font-medium rounded-md border-gray-300 transition-all duration-200`}>
+                    <div className='flex justify-between gap-2'>
+                        <h2 className='font-semibold text-lg'>Filters</h2>
+                        <button
+                            onClick={() => {
+                                setBrand("");
+                                setService("");
+                                setWarranty("");
+                                setMinPrice(0);
+                                setMaxPrice(0);
+                                setMinInput("");
+                                setMaxInput("");
+                                setShowMenu(false)
+                            }}
+                            className="btn btn-sm bg-transparent text-orange-600 hover:bg-gray-300 hover:text-gray-900"
+                        >
+                            Clear All
+                        </button>
+                        <button onClick={() => setShowMenu(false)} className="btn btn-sm text-orange-600"><IoMdCloseCircleOutline size={20} /></button>
+                    </div>
+
+                    {/* categories  */}
+                    <div className='mt-4'>
+                        <h2 className='font-semibold mb-2'>Categories</h2>
+                        <div className='flex flex-col gap-2 text-sm'>
+                            {
+                                categories.map(category => (
+                                    <Link
+                                        to={`/${category?.name}`}
+                                        onClick={() => { setBrand(""); setService(""); setWarranty(""); setMinPrice(0); setMaxPrice(0); setMinInput(""); setMaxInput(""); }}
+                                        key={category._id}
+                                        className={` ${category?.name === decodedCategory ? "active" : ""} border border-gray-300 rounded-md p-1`}
+                                    >
+                                        {category?.name}
+                                    </Link>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div className='border-b border-gray-300 my-4'></div>
+
+                    {/* brands  */}
+                    <div className='mt-4'>
+                        <h2 className='font-semibold mb-2'>Brands</h2>
+                        <div className='flex flex-col gap-2 text-sm'>
+                            {uniqueBrands.map((b, index) => (
+                                <Link
+                                    key={index}
+                                    onClick={() => setBrand(b)}
+                                    className={`border border-gray-300 rounded-md p-1 ${brand === b ? "active" : ""}`}
+                                >
+                                    {b}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                    <div className='border-b border-gray-300 my-4'></div>
+
+                    {/* Price  */}
+                    <div className='mt-4'>
+                        <h2 className='font-semibold mb-2'>Price</h2>
+                        <form onSubmit={handlePrice} action="#">
+                            <div className="flex gap-2 text-sm">
+                                <input
+                                    name="min"
+                                    type="number"
+                                    className="input input-xs"
+                                    placeholder="Min"
+                                    value={minInput}
+                                    onChange={(e) => setMinInput(e.target.value)}
+                                />
+                                <span>-</span>
+                                <input
+                                    name="max"
+                                    type="number"
+                                    className="input input-xs"
+                                    placeholder="Max"
+                                    value={maxInput}
+                                    onChange={(e) => setMaxInput(e.target.value)}
+                                />
+                                <button type="submit" className="btn btn-xs bg-orange-600 text-white text-lg">
+                                    <IoMdArrowDroprightCircle />
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className='border-b border-gray-300 my-4'></div>
+
+                    {/* Service & Promotion  */}
+                    <div className='mt-4'>
+                        <h2 className='font-semibold mb-2'>Service & Promotion</h2>
+                        <div className='flex gap-2 text-sm'>
+                            <Link
+                                onClick={() => { setService("free-delivery") }}
+                                className={`${service === "free-delivery" ? "active" : ""} w-full border border-gray-300 rounded-md p-1`}
+                            >
+                                Free Delivery
+                            </Link>
+                        </div>
+                    </div>
+                    <div className='border-b border-gray-300 my-4'></div>
+
+                    {/* Warranty Type  */}
+                    <div className='mt-4'>
+                        <h2 className='font-semibold mb-2'>Warranty Type</h2>
+                        <div className='flex flex-col gap-2 text-sm'>
+                            {["No Warranty", "3 Months", "6 Months", "1 Year", "2 Year"].map(w => (
+                                <Link
+                                    key={w}
+                                    onClick={() => setWarranty(w)}
+                                    className={`${warranty === w ? "active" : ""} w-full border border-gray-300 rounded-md p-1`}
+                                >
+                                    {w}
+                                </Link>
+                            ))}
+                        </div>
+                    </div>
+                    <div className='border-b border-gray-300 my-4'></div>
+                </div>
+
                 {/* main content  */}
                 <div className='lg:col-span-3 xl:col-span-4 p-2 rounded-md border border-gray-300'>
-                    <div className='mb-4 flex justify-between items-center gap-4 '>
+                    <div className='mb-4 flex flex-col md:flex-row justify-between md:items-center gap-4 '>
                         <div>
                             <h2 className='text-xl font-semibold'>{decodedCategory} Products</h2>
                             <p>{products.length} products found</p>
                         </div>
-                        <div>
-                            <div className="dropdown">
-                                <div tabIndex={0} role="button" className="btn m-1 min-h-0 h-auto py-2">Sort By Price</div>
-                                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-full p-2 shadow-sm">
-                                    <li><a>Low to high</a></li>
-                                    <li><a>High to low</a></li>
-                                </ul>
+                        <div className='flex gap-2 justify-between items-center'>
+                            <select defaultValue="Pick a color" className="select select-sm max-w-40">
+                                <option disabled={true}>Sort By</option>
+                                <option>Low to high</option>
+                                <option>High to</option>
+                                <option>Velvet</option>
+                            </select>
+                            <div>
+                                <button onClick={() => setShowMenu(!showSideMenu)} className="btn btn-sm"><CiFilter size={20} /></button>
                             </div>
                         </div>
                     </div>
